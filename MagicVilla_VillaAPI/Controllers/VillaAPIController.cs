ae48ce0e3b9deb0da6795222e2ad013743867d87
vijaybabar <babarvm@gmail.com>
2023-10-15 13:56:03 +0530

    Created first GET Endpoint

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,22 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]        
+        public IEnumerable<Villa> GetVillas()
+        {
+            var villas = new List<Villa>()
+            {
+                new Villa {Id=1,Name="Pool View"},
+                new Villa {Id=2,Name="Beach View"}
+            };
+
+            return villas;
+        }
+    }
+}
