325ab7439f1e64888ea8eb3c2a767dc6e77447b3
vijaybabar <babarvm@gmail.com>
2023-10-15 22:27:58 +0530

    Extended Model

--- MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
@@ -8,6 +8,15 @@ namespace MagicVilla_VillaAPI.Models.Dto
 
         [Required]
         [MaxLength(500)]
-        public string Name { get; set; }        
+        public string Name { get; set; }
+
+        public string Details { get; set; }
+
+        [Required]
+        public double Rate { get; set; }
+        public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
