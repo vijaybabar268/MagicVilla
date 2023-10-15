27fd681fa5d6e4204439f2eebf98e7ea49dd4ccc
vijaybabar <babarvm@gmail.com>
2023-10-15 17:44:58 +0530

    Model Validation

--- MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDto.cs
@@ -1,8 +1,13 @@
-﻿namespace MagicVilla_VillaAPI.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.Dto
 {
     public class VillaDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
         public string Name { get; set; }        
     }
 }
