085c0eb230bfa249700fcf668fe7ff45f09b6801
vijaybabar <babarvm@gmail.com>
2023-10-17 00:08:32 +0530

    Added RepositoryLayer & Implement Villa Repository

---
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,18 @@
+﻿using MagicVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MagicVilla_VillaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<IEnumerable<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true);
+
+        Task Create(Villa enity);
+
+        Task Remove(Villa enity);
+
+        Task Save();
+    }
+}
