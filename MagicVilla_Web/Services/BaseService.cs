494ae24e4ff2530e0e321a15aa309cc4e3838343
vijaybabar <babarvm@gmail.com>
2023-10-21 00:56:05 +0530

    Setup Base Service for API in Web Application

---
+++ MagicVilla_Web/Services/BaseService.cs
@@ -0,0 +1,80 @@
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MagicVilla_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new();
+            this.httpClient = httpClient;
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("MagicAPI");
+                var message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                if(apiRequest.Data != null)
+                {
+                    message.Content = 
+                        new StringContent(JsonConvert
+                        .SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+
+                switch(apiRequest.ApiType)
+                {
+                    case StaticDetail.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+
+                    case StaticDetail.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+
+                    case StaticDetail.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                HttpResponseMessage apiResponse = null;
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+
+                return APIResponse;
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string> { ex.Message.ToString() },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+
+                return APIResponse;
+            }
+        }
+    }
+}
