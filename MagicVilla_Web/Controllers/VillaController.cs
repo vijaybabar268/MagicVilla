31d722a7205831ccc05013041a27b82267f634a2
vijaybabar <babarvm@gmail.com>
2023-10-21 18:01:21 +0530

    Calling Villa API

---
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+
+        public VillaController(IVillaService villaService)
+        {
+            _villaService = villaService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDto> list = new();
+
+            var response = await _villaService.GetAllAsync<APIResponse>();
+            if(response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDto>>(response.Result.ToString());
+            }
+
+            return View(list);
+        }
+    }
+}
