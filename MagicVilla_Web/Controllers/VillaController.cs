209ae7a1d5d5dc463a09489c85725d4e9d3cf86f
vijaybabar <babarvm@gmail.com>
2023-10-21 18:16:08 +0530

    Display all Villa List

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
-using MagicVilla_VillaAPI.Models.Dto;
 using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -10,10 +10,12 @@ namespace MagicVilla_Web.Controllers
     public class VillaController : Controller
     {
         private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
 
-        public VillaController(IVillaService villaService)
+        public VillaController(IVillaService villaService, IMapper mapper)
         {
             _villaService = villaService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
