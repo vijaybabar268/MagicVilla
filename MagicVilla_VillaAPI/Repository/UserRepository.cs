b670f278be9f822f334b2e0552ad4fd432ff48f9
vijaybabar <babarvm@gmail.com>
2023-11-25 15:08:10 +0530

    Implement User Repository with Register, Login methods with JWT Token

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,83 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private string secretKey;
+
+        public UserRepository(ApplicationDbContext db, IConfiguration config)
+        {
+            _db = db;
+            secretKey = config.GetValue<string>("ApiSettings:Secret");
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            var userInDb = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == username.ToLower());
+            if(userInDb == null)
+                return true;
+            else 
+                return false;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            var user = await _db.LocalUsers.FirstOrDefaultAsync(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower() && u.Password == loginRequestDto.Password);
+            if(user == null)
+            {
+                return new LoginResponseDto()
+                {
+                    Token = null,
+                    User = user
+                };
+            }
+            // Generate GWT Token Via secreat key which is used to encrypt and decrypt
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(2),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            LoginResponseDto loginResponseDto = new LoginResponseDto()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+
+            return loginResponseDto;
+        }
+
+        public async Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            var user = new LocalUser()
+            {
+                UserName = registrationRequestDto.UserName,
+                Password = registrationRequestDto.Password,
+                Name = registrationRequestDto.Name,
+                Role = registrationRequestDto.Role
+            };
+
+            await _db.LocalUsers.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
